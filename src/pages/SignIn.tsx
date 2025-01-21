993f4da43acf93173fa103532eaabdf8e26b6fa2
nivaldoandrade <nivaldoandradef@gmail.com>
2025-01-21 14:35:13 -0300

    feat: integrate react-hook-form for form handling
    - Added react-hook-form as a dependency for managing form state
    - Updated SignIn page to use react-hook-form for email and password inputs
    - Defined an IFormData interface for type in form data
    - Implemented handleSubmit  to handle a form
    - Enhanced input components to register with react-hook-form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,22 +1,42 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
 
+interface IFormData {
+  email: string;
+  password: string
+}
 
 export function SignIn() {
+
+  const form = useForm<IFormData>({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const handleSubmit = form.handleSubmit(async ({ email, password }) => {
+    try {
+      console.log({ email, password });
+    } catch {
+      console.log('error');
+    }
+  });
 
   return (
     <div className="min-h-screen flex flex-col justify-center mx-auto max-w-[480px] p-6">
       <h1 className="font-semibold text-xl">Acesse sua conta</h1>
 
-      <form className='flex flex-col mt-8 gap-2'>
+      <form onSubmit={handleSubmit} className='flex flex-col mt-8 gap-2'>
         <div className="space-y-1">
           <Label htmlFor="email">E-mail</Label>
-          <Input id="email" />
+          <Input id="email" {...form.register('email')} />
         </div>
         <div className="space-y-1">
           <Label htmlFor="password">Senha</Label>
-          <Input id="password" />
+          <Input id="password" {...form.register('password')} />
         </div>
 
         <Button className='mt-3'>
