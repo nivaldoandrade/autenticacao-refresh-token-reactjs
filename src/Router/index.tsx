d7e9a0a1f8b7d048643370895d180953066facf6
nivaldoandrade <nivaldoandradef@gmail.com>
2025-01-28 14:23:55 -0300

    feat: add fallback route to redirect unknow paths to home
    - Added a wildcard router in the Router component to handle undefined paths
    - Ensures users are redirected to the home page if the access an invalid route

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,6 +1,6 @@
 import { Home } from '@/pages/Home';
 import { SignIn } from '@/pages/SignIn';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthGuard } from './AuthGuard';
 
 export function Router() {
@@ -14,6 +14,8 @@ export function Router() {
       <Route element={<AuthGuard isPrivate={false} />}>
         <Route path='/sign-in' element={<SignIn />} />
       </Route>
+
+      <Route path='*' element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
